103222afdae1a8b3dc2eae58158af24a28091094
Nhat Nguyen Van (SM) <nvnhat.17ck1@gmail.com>
2025-03-21 16:41:44 +0700

    update default theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -55,7 +55,7 @@ export default function RootLayout({
       >
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
+          defaultTheme="light"
           enableSystem
           disableTransitionOnChange
         >
