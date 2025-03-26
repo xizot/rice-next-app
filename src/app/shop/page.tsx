b4c33a22a8cb11bd4fa10c8fee9e6431163da61a
Nhat Nguyen Van (SM) <nvnhat.17ck1@gmail.com>
2025-03-26 12:44:15 +0700

    Update page.tsx

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -1,5 +1,6 @@
 import ProductList from "@/components/ProductList";
 import Section from "@/components/Section";
+import { Button } from "@/components/ui/button";
 import { Checkbox } from "@/components/ui/checkbox";
 import { Input } from "@/components/ui/input";
 import {
@@ -11,7 +12,9 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from "@/components/ui/sheet";
 import { topSellingRices } from "@/configs/mock-data.config";
+import { FilterIcon } from "lucide-react";
 
 const categories = [
   {
@@ -58,8 +61,8 @@ const categories = [
 
 export default function ShopPage () {
   return (
-    <div className="container mx-auto flex">
-      <div className="w-[295px] bg-background border-r pr-6">
+    <div className="container mx-auto flex max-md:pt-6 max-md:!px-0">
+      <div className="hidden md:block w-[295px] bg-background border-r pr-6">
         <div className="space-y-6 pt-6">
           <p className="mb-2 font-medium">Gõ tên gạo vào đây</p>
           <Input placeholder="Tìm kiếm" type="search" />
@@ -74,8 +77,8 @@ export default function ShopPage () {
           </div>
         </div>
       </div>
-      <Section className="flex-[1] pl-6">
-        <div className="flex justify-end ">
+      <div className="flex-[1] md:pl-6 md:pt-6">
+        <div className="flex justify-end gap-2 max-md:px-4">
           <Select>
             <SelectTrigger className="w-[180px]">
               <SelectValue placeholder="Sắp xếp" />
@@ -90,14 +93,40 @@ export default function ShopPage () {
               </SelectGroup>
             </SelectContent>
           </Select>
+          <Sheet>
+            <SheetTrigger asChild>
+              <Button variant="outline" className="md:hidden">
+                <FilterIcon className="h-4 w-4" />
+                <span className="ml-2">Lọc</span>
+              </Button>
+            </SheetTrigger>
+            <SheetContent>
+              <SheetHeader>
+                <SheetTitle>Bộ lọc</SheetTitle>
+              </SheetHeader>
+              <div className="space-y-6 p-4">
+                <p className="mb-2 font-medium">Gõ tên gạo vào đây</p>
+                <Input placeholder="Tìm kiếm" type="search" />
+                <div className="space-y-2">
+                  <p className="mb-2 font-medium">Các loại gạo khác</p>
+                  {categories.map((category) => (
+                    <div key={category.id} className="flex items-center space-x-2">
+                      <Checkbox id={category.name} />
+                      <label htmlFor={category.name}>{category.name}</label>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </SheetContent>
+          </Sheet>
         </div>
         <ProductList
           products={[...topSellingRices, ...topSellingRices]}
           size="small"
           className="lg:grid-cols-3"
-          sectionClassName="lg:py-6"
+          sectionClassName="py-4 lg:py-6"
         />
-      </Section>
+      </div>
     </div>
   );
 }
